5fb969ed0b19a44b2a3b1c54bea9e116fca74f9e
Dmytro Kovtun <dmytro_kovtun@epam.com>
2015-03-16 17:13:00 +0200

    Implementation of object extension method, which can deeply clone custom object.

---
+++ CloneObjectExtension/CloneObjectExtension/CloneExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneObjectExtension
+{
+    public static class CloneExtension
+    {
+        public static object Clone(this object objSource)
+        {
+            Type typeSource = objSource.GetType();
+            object objTarget = Activator.CreateInstance(typeSource);
+            const BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+            PropertyInfo[] propertyInfo = typeSource.GetProperties(flags);
+            foreach (PropertyInfo property in propertyInfo)
+            {
+                if (property.CanWrite)
+                {
+                    if (property.PropertyType.IsValueType || property.PropertyType.IsEnum || property.PropertyType == typeof(System.String))
+                    {
+                        property.SetValue(objTarget, property.GetValue(objSource, null), null);
+                    }
+                    else
+                    {
+                        object objPropertyValue = property.GetValue(objSource, null);
+                        property.SetValue(objTarget, objPropertyValue == null ? null : objPropertyValue.Clone(), null);
+                    }
+                }
+            }
+            return objTarget;
+        }
+    }
+}
---
+++ CloneObjectExtension/CloneObjectExtension/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneObjectExtension
+{
+    public class TestObjectA
+    {
+        public static int a;
+        protected const long b = 2;
+
+        private string c;
+        public int d;
+
+        private TestObjectB tob;
+
+        public TestObjectA()
+        {
+            a = 1;
+            d = 4;
+            c = "aba";
+            tob = new TestObjectB();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("a={0} b={1} c={2} d={3} tob: {4}", a, b, c, d, tob);
+        }
+    }
+
+    public class TestObjectB
+    {
+        public static int e;
+        protected const long f = 6;
+
+        private string g;
+        public int h;
+        private long[] mas;
+
+        public TestObjectB()
+        {
+            e = 5;
+            g = "caba";
+            h = 8;
+            mas = new[] {9L, 10};
+        }
+
+        public override string ToString()
+        {
+            return string.Format("e={0} f={1} g={2} h={3}", e, f, g, h);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestObjectA testObject = new TestObjectA();
+            object clonedTestObject = testObject.Clone();
+            Console.WriteLine(testObject);
+            Console.WriteLine(clonedTestObject);
+        }
+    }
+}
