357a98b3d9ae1a73ab265974766a4a08e8e779b5
Dmytro Kovtun <dmytro_kovtun@epam.com>
2015-09-11 12:07:08 +0300

    Added integration tests

---
+++ SignalRChat/SignalRChat.Integration.Tests/AppConfig.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecsFor.Mvc;
+
+namespace SignalRChat.Integration.Tests
+{
+    [TestClass]
+    public class AppConfig
+    {
+        private static SpecsForIntegrationHost _integrationHost;
+
+        [AssemblyInitialize()]
+        public static void MyAssemblyInitialize(TestContext testContext)
+        {
+            var config = new SpecsForMvcConfig();
+
+            config.UseIISExpress()
+                .With(Project.Named("SignalRChat"))
+                .ApplyWebConfigTransformForConfig("Debug");
+
+            config.BuildRoutesUsing(RouteConfig.RegisterRoutes);
+
+            config.UseBrowser(BrowserDriver.Chrome);
+
+            _integrationHost = new SpecsForIntegrationHost(config);
+            _integrationHost.Start();
+        }
+
+        [AssemblyCleanup()]
+        public static void MyAssemblyCleanup()
+        {
+            _integrationHost.Shutdown();
+        }
+    }
+}
---
+++ SignalRChat/SignalRChat.Integration.Tests/SignalRIntegrationTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Web.SessionState;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using SignalRChat.Controllers;
+using SignalRChat.Models;
+using SpecsFor.Mvc;
+using Should;
+
+
+namespace SignalRChat.Integration.Tests
+{
+    [TestClass]
+    public class SignalRIntegrationTests : SpecsFor.SpecsFor<MvcWebApp>
+    {
+        private static MvcWebApp _app;
+
+        [ClassInitialize]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            _app = new MvcWebApp();
+        }
+
+        [TestMethod]
+        public void AuthorShouldBeInvalidWhenEmpty()
+        {
+            _app.NavigateTo<HomeController>(c => c.Index());
+            _app.FindFormFor<Message>()
+                .Field(f => f.Author).SetValueTo(string.Empty)
+                .Field(f => f.Text).SetValueTo("TestMessage");
+
+            _app.Browser.FindElement(By.Id("send-btn")).Click();
+
+            _app.FindFormFor<Message>().Field(f => f.Author).ShouldBeInvalid();
+        }
+
+        [TestMethod]
+        public void TextFieldShouldBeEmptyAfterSendingMessage()
+        {
+            _app.NavigateTo<HomeController>(c => c.Index());
+            _app.FindFormFor<Message>()
+                .Field(f => f.Author).SetValueTo("TestAuthor")
+                .Field(f => f.Text).SetValueTo("TestMessage");
+
+            _app.Browser.FindElement(By.Id("send-btn")).Click();
+
+            _app.FindFormFor<Message>().Field(f => f.Author).ShouldNotBeNull();
+            _app.Browser.FindElement(By.Id("Text")).Text.ShouldBeSameAs("");
+        }
+    }
+}
